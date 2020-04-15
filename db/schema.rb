00643d63a6e88b0b2b4688de212997160be4d079
vagrant <jinsan0031@gmail.com>
2020-04-15 14:18:21 +0000

    model追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_082129) do
+ActiveRecord::Schema.define(version: 2020_04_15_141626) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "item_id"
+    t.integer "item_quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.text "description"
+    t.string "name"
+    t.integer "price"
+    t.string "image_id"
+    t.boolean "is_sale_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "product_status"
+    t.integer "price"
+    t.integer "quantity"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "pay_method"
+    t.integer "status"
+    t.integer "freight"
+    t.integer "total"
+    t.string "ship_name"
+    t.string "ship_postcode"
+    t.string "ship_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ships", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "address"
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
