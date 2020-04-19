b91a42126a562862ec8182262ba0681c1067275c
NYuya <noooochan5@gmail.com>
2020-04-19 11:14:04 +0000

    更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,43 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+
 if Rails.env == "development"
   (1..9).each do |i|
-    Customer.create!(last_name: "test#{1}", first_name: "test#{1}", last_name_kana: "test#{1}", first_name_kana: "test#{1}", post_code: "123123#{1}", address: "test#{1}", phone_number: "1234567890#{1}",email: "test#{i}@example.com", password: "password", password_confirmation: "password")
+    Item.create!(genre_id: "100#{i}", description: "test#{i}", name: "test#{i}", price: "100#{i}", image_id: "test#{i}", is_sale_status: "test#{i}")
   end
 end
 
 
 if Rails.env == "development"
   (1..9).each do |i|
-    Ship.create!(customer_id: "#{1}", address: "test#{1}", name: "test#{1}", code: "123456#{1}")
-  end
-end
-
-
-if Rails.env == "development"
-  (1..9).each do |i|
-    Genre.create!(name: "test#{1}", is_active: "test#{1}")
-  end
-end
-
-
-if Rails.env == "development"
-  (1..9).each do |i|
-    Item.create!(genre_id: "test#{1}", description: "test#{1}", name: "test#{1}", price: "100#{1}", image_id: "test#{1}", is_sale_status: "test#{1}")
-  end
-end
-
-
-if Rails.env == "development"
-  (1..9).each do |i|
-    OrderItem.create!(item_id: "test#{1}", order_id: "test#{1}", product_status: "test#{1}", price: "test#{1}", quantity: "test#{1}", name: "test#{1}")
-  end
-end
-
-if Rails.env == "development"
-  (1..9).each do |i|
-    Admin.create!(email: "test#{i}@example.com", password: "password")
-
+    OrderItem.create!(item_id: "item#{i}", order_id: "order#{i}", product_status: "test#{i}", price: "50#{i}", quantity: "test#{i}", name: "test#{i}")
   end
 end
