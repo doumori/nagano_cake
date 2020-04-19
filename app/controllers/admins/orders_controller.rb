3fa478a10a8c183ba2d975080e686cc929523553
NYuya <60781603+NYuya@users.noreply.github.com>
2020-04-20 01:59:51 +0900

    Update orders_controller.rb

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -4,29 +4,13 @@ class Admins::OrdersController < ApplicationController
   end
 
   def show
-<<<<<<< HEAD
-  	@order=Order.find(params[:id])
-    @order_items=@order.order_items
-    @sum=100
-    @freight=800
-    @total=@freight+@sum
-=======
     @order_items = OrderItem.all
     @order = Order.find(params[:id])
     @item = Item.find(params[:id])
     @order_item = OrderItem.find(params[:id])
->>>>>>> master
   end
-
 
   def update
-<<<<<<< HEAD
-  	@cart_item=CartItem.find(params[:cart_item_id])
-  end
-  private
-  def order_params
-  	params.require(:order).permit(:customer_id, :total,:freight, :total, :pay_method, :status,:ship_name,:ship_postcode,:ship_address)
-=======
   	@order = Order.find(params[:id])
     @order.update(order_params)
     redirect_to admins_orders_path
@@ -39,6 +23,5 @@ class Admins::OrdersController < ApplicationController
   private
   def order_params
   	params.require(:order).permit(:customer_id, :pay_method, :status, :freight, :total, :ship_name, :ship_postcode, :ship_address)
->>>>>>> master
   end
 end
