90ec9279da9f83cbee2c6c01b35ea1ce6abd2992
NYuya <noooochan5@gmail.com>
2020-04-19 08:11:29 +0000

    Admin ジャンル、顧客ページ

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -6,9 +6,9 @@ class Admins::GenresController < ApplicationController
   end
 
   def create
-  	@newgenre = Genre.new
-  	if @newgenre.save #入力されたデータをdbに保存する。
-  		redirect_to admins_genres_path, notice: "successfully created Genre!"#保存された場合の移動先を指定。
+  	@newgenre = Genre.new(genre_params)
+  	if @newgenre.save
+  		redirect_to admins_genres_path, notice: "successfully created Genre!"
   	else
   		@genres = Genres.all
   		render admins_genres_path
@@ -16,28 +16,25 @@ class Admins::GenresController < ApplicationController
   end
 
   def edit
-  	@genre=Genre.find(params[:id])
-  end
-  private
-   def genre_params
-    params.require(:genre).permit(:name,:is_active)
   	@genre = Genre.find(params[:id])
   end
 
   def update
     @genre = Genre.find(params[:id])
-    if @genre.save #入力されたデータをdbに保存する。
-      redirect_to admins_genres_path, notice: "successfully created Genre!"#保存された場合の移動先を指定。
-    else
-      @genres = Genres.all
-      render admins_genres_path
-    end
+    @genre.update(genre_params)
+    redirect_to admins_genres_path, notice: "successfully created Genre!"
   end
 
   def destroy
     @genre = Genre.find(params[:id])
     @genre.destroy
     redirect_to admins_genres_path, notice: "successfully delete genre!"
+  end
+
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
   end
 
 end
