ed4469c45060bd4ad60d9e0079301c7eca00e217
tosswi <60781002+tosswi@users.noreply.github.com>
2020-04-25 17:30:06 +0900

    Update order_items_controller.rb

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -6,14 +6,10 @@ class Admins::OrderItemsController < ApplicationController
     @order_item.update(order_item_params)
 
     @order_item.order.order_items.each_with_index do |o_i, index|
-      p "製作完了"
-      p index
-      p o_i.product_status
+
     unless o_i.product_status=="製作完了"
-      p "まだ製作中の製品あり"
       break
        end
-       #o_iが一週なら
        if index == @order_item.order.order_items.size - 1
         @order_item.order.update(status: "発送準備中")
       end
